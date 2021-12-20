717ddea8f8d523d76bc0d1a239e650a51c8a14f3
onur.bektas <onur.bektas@obss.com.tr>
2021-12-20 22:55:21 +0300

    feign client lı meeting

--- schedule-with-me-fe/src/views/pages/dashboard/transaction.vue
+++ schedule-with-me-fe/src/views/pages/dashboard/transaction.vue
@@ -1,48 +1,19 @@
 <script>
-/**
- * Transaction component
- */
+import {meetingService} from "@/helpers/fakebackend/meeting.service";
 export default {
   data() {
     return {
-      transactionData: [
-        {
-          profile: require("@/assets/images/users/avatar-2.jpg"),
-          name: "Herbert C. Patton",
-          status: "Confirm",
-          amount: "$14,584",
-          date: "5/12/2016",
-        },
-        {
-          profile: require("@/assets/images/users/avatar-3.jpg"),
-          name: "Mathias N. Klausen",
-          status: "Waiting payment",
-          amount: "$8,541",
-          date: "10/11/2016",
-        },
-        {
-          profile: require("@/assets/images/users/avatar-4.jpg"),
-          name: "Nikolaj S. Henriksen",
-          status: "Confirm",
-          amount: "$954",
-          date: "8/11/2016",
-        },
-        {
-          profile: require("@/assets/images/users/avatar-5.jpg"),
-          name: "Lasse C. Overgaard",
-          status: "Payment expired",
-          amount: "$44,584",
-          date: "7/11/2016",
-        },
-        {
-          profile: require("@/assets/images/users/avatar-6.jpg"),
-          name: "Kasper S. Jessen",
-          status: "Confirm",
-          amount: "$8,844",
-          date: "1/11/2016",
-        },
-      ],
+      reportData: [],
     };
+  },
+  mounted() {
+
+    this.meetingService.getMeetingOrganizerStatistics()
+  },
+  methods:{
+    getOrganizationReport(){
+
+    },
   },
 };
 </script>
@@ -51,26 +22,13 @@ export default {
   <div class="col-xl-6">
     <div class="card">
       <div class="card-body">
-        <div class="float-end">
-          <b-dropdown toggle-class="text-reset p-0" variant="white" menu-class="dropdown-menu-end">
-            <template #button-content>
-              <span class="fw-semibold">Sort By:</span>
-              <span class="text-muted"
-              >Recent<i class="mdi mdi-chevron-down ms-1"></i
-              ></span>
-            </template>
-            <b-dropdown-item>Maximum</b-dropdown-item>
-            <b-dropdown-item>Recent</b-dropdown-item>
-            <b-dropdown-item>Minimum</b-dropdown-item>
-          </b-dropdown>
-        </div>
 
         <h4 class="card-title mb-4">Top Conversation Created Users</h4>
 
         <div class="table-responsive">
           <table class="table align-middle table-striped table-nowrap mb-0">
             <tbody>
-            <tr v-for="(item, index) in transactionData" :key="index">
+            <tr v-for="(item, index) in reportData" :key="index">
               <td>
                 <img
                     :src="`${item.profile}`"
